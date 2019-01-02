94dc5ba737afcbebe107ff65e43647dc98c9f8ac
abdullah6265 <abdullah@reactivespace.com>
2019-01-02 16:52:07 +0500

    ost

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Client = require('bitcoin-core');
-const client = new Client({user:'bitcoin123',pass:'btcnode123',port: 8332});
+const client = new Client({
+    user:'bitcoin123',
+    pass:'btcnode123',
+    port: 8332,
+host:'127.0.0.1'});
     client.getInfo((error, help) => console.log(help));
