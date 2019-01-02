d5e73005e2ccb7c20d8c03aa28a1c45dbda58d07
abdullah6265 <abdullah@reactivespace.com>
2019-01-02 13:06:49 +0500

    client info

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Client = require('bitcoin-core');
-const client = new Client({ username:'',password:'',network: 'regtest',port: 28332,ssl: {
+const client = new Client({ rpcuser:'bitcoin123',rpcpassword:'btcnode123',network: 'regtest',port: 28332,ssl: {
     enabled: true,
     strict: false} });
     client.getInfo((error, help) => console.log(error));
