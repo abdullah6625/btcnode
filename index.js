e6c1dff7af6da9f280953d0428112c985801b122
abdullah6265 <abdullah@reactivespace.com>
2019-01-02 17:16:21 +0500

    ds

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const client = new Client({
     port: 8332,
 host:'127.0.0.1'});
 client.getInfo().then((help) => console.log(help)).catch((Err)=>{
+    console.log('helllooo');
     console.log(Err);
 })
