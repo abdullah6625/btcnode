bdf2dc36c7bb9c1ec8980b2f87e8765a10bf8001
abdullah6265 <abdullah@reactivespace.com>
2019-01-02 13:27:41 +0500

    networlk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Client = require('bitcoin-core');
-const client = new Client({ user:'bitcoin123',pass:'btcnode123',network: 'regtest',port: 28332,ssl: {
+const client = new Client({ user:'bitcoin123',pass:'btcnode123',port: 28332,ssl: {
     enabled: true,
     strict: false} });
     client.getInfo((error, help) => console.log(error));
