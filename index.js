88e8b640fdafc2108b27c19933970941a339ce80
abdullah6265 <abdullah@reactivespace.com>
2019-01-02 17:54:01 +0500

    dssd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Client = require('bitcoin-core');
 const client = new Client({
-    username:'bitcoin123',
-    password:'btcnode123',
+    user:'bitcoin123',
+    pass:'btcnode123',
     port: 8332,
 host:'127.0.0.1'});
 client.getInfo().then((help) => console.log(help)).catch((Err)=>{
